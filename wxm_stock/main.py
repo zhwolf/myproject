8a5ba462d1782c035259260e7789c7c618046600
zhwolf <zhwolf_zj@163.com>
2014-09-17 23:46:10 +0800

    edit stocks

--- wxm_stock/main.py
+++ wxm_stock/main.py
@@ -4,7 +4,7 @@
 import os
 import web
 from jinja2 import Environment,FileSystemLoader
-
+import utils
 web.config.debug = True
 
 ### urls
@@ -13,7 +13,8 @@ urls = (
 #####
     "^//*", "index",
     "^/edit/*", "edit",
-    "^/edit/stock/(\d+)/*", "UpdateStock",
+    "^/edit/stock/(.*)/edit/*", "UpdateStock",
+    "^/edit/stock/(.*)/del/*", "RemoveStock",
     "^/record/*", "record",
     "^/history/*", "history",
     )
@@ -46,32 +47,75 @@ class edit:
     def GET(self):
         return render_template("edit.html")
 
+    def POST(self):
+        inputs = web.input()
+        wheres = {}
+        a = inputs.get('query_code', '')
+        if  a!= '':
+            wheres['stocks.code'] = a
+        else:
+            a = inputs.get('query_zone', '')
+            if a !='':
+                wheres['stocks.zone'] = a
+            a = inputs.get('query_industry', '')
+            if a !='':
+                wheres['stocks.industry'] = a
+            a = inputs.get('query_subindustry', '')
+            if a !='':
+                wheres['stocks.subindustry'] = a
+        datas = db.where('stocks', limit=100, **wheres)
+
+        return render_template("edit.html", datas=datas)
+
+class RemoveStock:
+    def GET(self, code):
+        code = code.strip()
+        if code != "":
+            db.delete('stocks', where="code=  $code",vars ={'code':code})
+        return web.seeother("/edit/")
+
 class UpdateStock:
     def GET(self, code):
         data = None
-        try:
-            code = int(code.strip())
-        except:
-            code = 0
-        if code != 0:
-            myvar = dict(code=code)
-            datas = db.select('stocks', myvar, where="id=  $code", limit=1)
+        code = code.strip()
+        if code != "":
+            datas = db.select('stocks', where="code=  $code",vars ={'code':code}, limit=1)
             for data in datas:
                 break
-        return render_template("updateStock.html", data=data)
+        return render_template("updateStock.html", data=data, queryurl="/edit/")
 
     def POST(self, code):
+        inputs = web.input()
+        id = inputs.get('id', '')
         try:
-            code = int(code.strip())
+            id = int(id)
         except:
-            code = 0
-        if code != 0:
-            myvar = dict(code=code)
-            datas = db.select('stocks', myvar, where="id=  $code", limit=1)
-            for data in datas:
-                break
+            id = 0
+        errs = ""
+        count = int(db.query("SELECT count(id) as stocks_total FROM stocks WHERE id !=$id and code= $code", vars={'id':id, 'code': inputs.get('code', '')})[0].stocks_total)
+        result = count ==0
+        if result:
+            myvars ={}
+            for field in utils.Table_Stocks:
+                if inputs.has_key(field):
+                    myvars[field] =inputs.get(field)
+            try:
+                if id <=0:
+                    db.insert('stocks', **myvars)
+                else:
+                    db.update('stocks', "id = $id", {'id':id},  **myvars)
+                result = True
+            except Exception as e:
+                result = False
+                errs = u"数据库错误!"
+        else:
+            errs = u"已经存在相同的股票代码!"
+        if result:
+            return web.seeother("/edit/")
+        else:
+            return render_template("updateStock.html", data=inputs, errs = errs, queryurl="/edit/")
 
-        data = web.data() # you can get data use this method
+
 
 class record:
     def GET(self):
---
+++ wxm_stock/utils.py
@@ -0,0 +1,116 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     16-09-2014
+# Copyright:   (c) Administrator 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import re,urllib2
+import logging
+
+class Any:
+    pass
+
+
+CHAIWU_MODELS=[
+        #市值  -- 市值，已发行股本，流通股
+        [
+            'shizhi',
+            'faxingguben',
+            'liutonggu',
+        ],
+        #每股财务数据 --收益，销售，帐面价值，现金流，现金
+        [
+            'shouyi',
+            'xiaoshou',
+            'zhangmian',
+            'xianjinliu',
+            'xianjin',
+        ],
+        #估值指标 --市盈率,总市值/销售收入,市帐率 （市价/帐面资产净值）,股价/每股现金
+        [
+            'shiyinglv',
+            'zhongshizhi',
+            'shizhanglv',
+            'gujia',
+        ],
+        #管理效率 --权益收益率,资产报酬率,投资回报率
+        [
+            'qyshouyilv',
+            'baochoulv',
+            'huibaolv',
+        ],
+        #盈利能力比率 --毛利,营运利润率,净利率
+        [
+            'maoli',
+            'lirunlv',
+            'jinglilv',
+        ],
+        #分红送配 --股息收益率,每股股利,股息支付率
+        [
+            'gxshouyilv',
+            'guli',
+            'zhifulv',
+        ],
+        #财务实力 --速动比率,流动比率,长期债务率,债务股本比
+        [
+            'sdbilv',
+            'ldbilv',
+            'zhaiwulv',
+            'zwgubenbi',
+        ],
+    ]
+
+Table_Stocks=[
+    "code",
+    "name",
+    "zone",
+    "industry",
+    "subindustry",
+    "reporttime",
+]
+
+Table_Report=[
+    "code",
+    "currency",
+    "name",
+    "price",
+    "change",
+    "shizhi",
+    "faxingguben",
+    "liutonggu",
+    "shouyi",
+    "xiaoshou",
+    "zhangmian",
+    "xianjinliu",
+    "xianjin",
+    "shiyinglv",
+    "zhongshizhi",
+    "shizhanglv",
+    "gujia",
+    "qyshouyilv",
+    "baochoulv",
+    "huibaolv",
+    "maoli",
+    "lirunlv",
+    "jinglilv",
+    "gxshouyilv",
+    "guli",
+    "zhifulv",
+    "sdbilv",
+    "ldbilv",
+    "zhaiwulv",
+    "zwgubenbi",
+    "TotalRevenue",
+    "GrossProfit",
+    "OperatingIncome",
+    "NetIncome",
+    "reporttime",
+]
+
